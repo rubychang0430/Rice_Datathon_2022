a7f0134f3ad25e01b34901000d21385c5d301e62
rubychang0430 <84561896+rubychang0430@users.noreply.github.com>
2022-02-04 21:46:54 -0600

    Update app.py
    Forecast visulization updated

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import streamlit as st
 import altair as alt
 from io import BytesIO
 from readtxt import convert_to_df, datetime_range, generate_datetime, final_df
+from forecast import datetime_new_range, generate_new_datetime, forecast
 
 st.set_page_config(layout="wide")
 
@@ -29,6 +30,9 @@ with st.sidebar:
 
     d = st.date_input("Select your data", datetime.date(2021, 12, 2))
     st.write('Your selected data is:', d)
+    
+    width = st.sidebar.slider("plot width", 0.1, 25., 5.)
+    height = st.sidebar.slider("plot height", 0.1, 25., 5.)
 
 st.markdown("")
 
@@ -48,21 +52,31 @@ st.line_chart(df['WDIR'])
 st.subheader('Wind Speed')
 st.line_chart(df['WSPD'])
 
+fc_WDIR,lower_WDIR,upper_WDIR,fc_WSPD,lower_WSPD,upper_WSPD = forecast(df)
+fc_WDIR.index = pd.to_datetime(fc_WDIR.index)
+fc_WSPD.index = pd.to_datetime(fc_WSPD.index)
+
 st.title('Forecast of wind speed and direction in the next 72 hours')
+st.subheader('Forecast of Wind Direction')
+# Plot
+fig = plt.figure(figsize=(12,5), dpi=100)
+plt.plot(df['WDIR'], label='training')
+plt.plot(fc_WDIR, label='forecast')
+plt.fill_between(lower_WDIR.index, lower_WDIR, upper_WDIR, color='k', alpha=.15)
+plt.legend(loc='upper left', fontsize=8)
+st.pyplot(fig)
+	
+st.subheader('Forecast of Wind Speed')
+fig = plt.figure(figsize=(12,5), dpi=100)
+plt.plot(df['WSPD'], label='training')
+plt.plot(fc_WSPD, label='forecast')
+plt.fill_between(lower_WSPD.index, lower_WSPD, upper_WSPD, color='k', alpha=.15)
+plt.legend(loc='upper left', fontsize=8)
+st.pyplot(fig)
+
 df['daytime']= pd.to_datetime(df['date']).dt.date
 df_day = df[df['daytime']==d]
-
-if df_day.shape[0] == 0:
-	st.error('Error: The date you enter is not in the dataset.')
-else:
-	st.subheader('Forecast of Wind Direction')
-
-	st.subheader('Forecast of Wind Speed')
-	
-	
 st.title('Daily average wind speed in the mean direction of the day you selected.')
-width = st.sidebar.slider("plot width", 0.1, 25., 3.)
-height = st.sidebar.slider("plot height", 0.1, 25., 1.)
 if df_day.shape[0] == 0:
 	st.error('Error: The date you enter is not in the dataset.')
 else:
